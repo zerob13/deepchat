7eb59cacaeb6a00ac7ee215596fb49faa9a33ce1
zerob13 <zerob13@gmail.com>
2025-03-03 15:42:12 +0800

    add notarize flag

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -2,6 +2,12 @@ import { notarize } from '@electron/notarize'
 
 export default async function notarizing(context) {
   const { electronPlatformName, appOutDir } = context
+  const releaseFlag = process.env.build_for_release
+  console.info('releaseFlag', releaseFlag)
+  if (!releaseFlag) {
+    console.info('Skipping notarization as build_for_release is not set')
+    return
+  }
   if (electronPlatformName !== 'darwin') {
     return
   }
